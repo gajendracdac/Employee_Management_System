88419a1679c7765ec28b5512f6c875bc28ae9d29
gajendracdac <gajendrapotter@gmail.com>
2022-11-17 20:57:17 +0530

    File updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,7 @@ class HelloWorld
     {
      System.out.println("Hello World");
      System.out.println("Welcome to Java");
+     int a=10,b=20;
+     System.out.println("Sum of "+a+" and "+b+" is "+(10+20));
      }
  }
